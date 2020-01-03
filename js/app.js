fb0747bec723c345e3e2ee7ceee4907958ece28d
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-01-03 10:52:17 +0200

    starter code of the app

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// import quotations list!
+import quotations from './quotations.js';
+
+// change the backgroudColor of generateButton when the mouse over and out! 
+const generateButton = document.querySelector('.btn');
+
+generateButton.addEventListener('mouseover', () => {
+    generateButton.style.backgroundColor= "crimson";
+})
+
+generateButton.addEventListener('mouseout', () => {
+    generateButton.style.backgroundColor= "chocolate";
+})
+
+
