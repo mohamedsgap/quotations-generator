9328425dc1d7d28bac1179bdd5c52c085f851966
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-01-04 03:59:46 +0200

    split quotations into two cat!

---
+++ js/sportQuotations.js
@@ -0,0 +1,107 @@
+const sportQuotations = [
+  {
+    quotation: "Hard work beats talent when talent doesn’t work hard.",
+    author: "Tim Notke",
+    category: "sport"
+  },
+  {
+    quotation: "It’s hard to beat a person who never gives up.",
+    author: "Babe Ruth",
+    category: "sport"
+  },
+  {
+    quotation: "The harder the battle, the sweeter the victory.",
+    author: "Les Brown",
+    category: "sport"
+  },
+  {
+    quotation:
+      "Never say never because limits, like fears, are often just an illusion.",
+    author: "Michael Jordan",
+    category: "sport"
+  },
+  {
+    quotation: "You miss 100% of the shots you don’t take.",
+    author: "Wayne Gretzky",
+    category: "sport"
+  },
+  {
+    quotation: "You have to expect things of yourself before you can do them.",
+    author: "Vince Lombardi",
+    category: "sport"
+  },
+  {
+    quotation: "Winning isn’t everything, but wanting to win is.",
+    author: "Derek Jeter",
+    category: "sport"
+  },
+  {
+    quotation:
+      "There may be people that have more talent than you, but there’s no excuse for anyone to work harder than you do.",
+    author: "Sandy Koufax",
+    category: "sport"
+  },
+  {
+    quotation:
+      "I became a good pitcher when I stopped trying to make them miss the ball and started trying to make them hit it.",
+    author: "Terry Bradshaw",
+    category: "sport"
+  },
+  {
+    quotation:
+      "When you’ve got something to prove, there’s nothing greater than a challenge.",
+    author: "Ben Hogan",
+    category: "sport"
+  },
+  {
+    quotation: "If you can’t outplay them, outwork them",
+    author: "William James",
+    category: "sport"
+  },
+  {
+    quotation:
+      "Most people never run far enough on their first wind to find out they’ve got a second.",
+    author: "Michael Jordan",
+    category: "sport"
+  },
+  {
+    quotation:
+      "I’ve failed over and over and over again in my life. And that is why I succeed.",
+    author: "Vince Lombardi",
+    category: "sport"
+  },
+  {
+    quotation:
+      "“It’s not whether you get knocked down; it’s whether you get up.",
+    author: "Mike Singletary",
+    category: "sport"
+  },
+  {
+    quotation:
+      "Do you know what my favorite part of the game is? The opportunity to play.",
+    author: "Pele",
+    category: "sport"
+  },
+  {
+    quotation:
+      "The more difficult the victory, the greater the happiness in winning.",
+    author: "George Halas",
+    category: "sport"
+  },
+  {
+    quotation: "Nobody who ever gave his best regretted it.",
+    author: "John Wooden",
+    category: "sport"
+  },
+  {
+    quotation: "Do not let what you can not do interfere with what you can do.",
+    author: "",
+    category: "sport"
+  },
+  {
+    quotation:
+      "One man practicing sportsmanship is far better than a hundred teaching it.",
+    author: "Knute Rockne",
+    category: "sport"
+  }
+];
---
+++ js/techQuotations.js
@@ -0,0 +1,121 @@
+const techQuotations = [
+  {
+    quotation:
+      "Any sufficiently advanced technology is indistinguishable from magic.",
+    author: "Arthur C. Clarke",
+    category: "tech"
+  },
+  {
+    quotation:
+      "You never change things by fighting the existing reality To change something, build a new model that makes the existing model obsolete.",
+    author: "Buckminster Fulle",
+    category: "tech"
+  },
+  {
+    quotation:
+      "We are stuck with technology when what we really want is just stuff that works.",
+    author: " Douglas Adams",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Please, no matter how we advance technologically, please don't abandon the book. There is nothing in our material world more beautiful than the book.",
+    author: "Patti Smith",
+    category: "tech"
+  },
+  {
+    quotation:
+      "There will come a time when it isn't 'They're spying on me through my phone' anymore. Eventually, it will be 'My phone is spying on me'.",
+    author: "Philip K. Dick",
+    category: "tech"
+  },
+  {
+    quotation:
+      "I know there's a proverb which that says 'To err is human,' but a human error is nothing to what a computer can do if it tries.",
+    author: " Agatha Christie",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Technological progress has merely provided us with more efficient means for going backwards.",
+    author: "Aldous Huxley",
+    category: "tech"
+  },
+  {
+    quotation:
+      "It's supposed to be automatic, but actually you have to push this button. ",
+    author: "John Brunner",
+    category: "tech"
+  },
+  {
+    quotation: "The iPod completely changed the way people approach music.",
+    author: "Karl Lagerfeld",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Even the technology that promises to unite us, divides us. Each of us is now electronically connected to the globe, and yet we feel utterly alone.",
+    author: " Dan Brown",
+    category: "tech"
+  },
+  {
+    quotation:
+      "It has become appallingly obvious that our technology has exceeded our humanity.",
+    author: "Albert Einstein",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Our belief was that if we kept putting great products in front of customers, they would continue to open their wallets.",
+    author: "Steve Jobs",
+    category: "tech"
+  },
+  {
+    quotation:
+      "The world won't care about your self-esteem. The world will expect you to accomplish something BEFORE you feel good about yourself.",
+    author: "Bill Gates",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution",
+    author: "Albert Einstein",
+    category: "tech"
+  },
+  {
+    quotation:
+      "We have to stop optimizing for programmers and start optimizing for users.",
+    author: "Jeff Atwood",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Technology is just a tool. In terms of getting the kids working together and motivating them, the teacher is the most important.",
+    author: "Bill Gates",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Technology feeds on itself. Technology makes more technology possible.",
+    author: "Alvin Toffler",
+    category: "tech"
+  },
+  {
+    quotation:
+      "The Web as I envisaged it, we have not seen it yet. The future is still so much bigger than the past.",
+    author: "Tim Berners-Lee",
+    category: "tech"
+  },
+  {
+    quotation:
+      "Once a new technology rolls over you, if you're not part of the steamroller, you're part of the road.",
+    author: "Stewart Brand",
+    category: "tech"
+  },
+  {
+    quotation:
+      "The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life.",
+    author: "Bill Gate",
+    category: "tech"
+  }
+];
