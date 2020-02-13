1e802ab3685377ec10c621edbec3a3650a15a869
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-13 10:55:38 +0200

    Add Quotes file

---
+++ 3-quotes-generator-react/src/Quotes.js
@@ -0,0 +1,80 @@
+export const quotes = {
+    tech: {
+        begin: [
+            "Any sufficiently advanced",
+            "You never change things by fighting",
+            "We are stuck with technology",
+            "Please no matter how we advance technologically",
+            "There will come a time when it isn't They are",
+            "I know there's a proverb which that says",
+            "Technological progress has merely",
+            "It's supposed to be automatic",
+            "The iPod completely",
+            "Even the technology that promises to unite us",
+        ],
+        middle: [
+            "technology is indistinguishable",
+            "the existing reality To change something",
+            "when what we really want is",
+            "please don't abandon the boo",
+            "spying on me through my phone anymore",
+            "To err is human but a human error is nothing",
+            "provided us with more efficient",
+            "but actually you have",
+            "changed the way",
+            "divides us Each of us is now electronically connected to the globe"
+        ],
+        end: [
+            "from magic",
+            "build a new model that makes the existing model obsolete",
+            "just stuff that works",
+            "There is nothing in our material world more beautiful than the book",
+            "Eventually it will be My phone is spying on me",
+            "to what a computer can do if it tries",
+            "means for going backwards",
+            "to push this button",
+            "people approach music",
+            "and yet we feel utterly alone"
+        ]
+    },
+    sport: {
+        begin: [
+            "Hard work beats talent",
+            "It’s hard to beat",
+            "Never say never because",
+            "You have to expect",
+            "There may be people that have more",
+            "I became a good pitcher when I",
+            "When you’ve got something",
+            "Most people never run far",
+            "I’ve failed over and over",
+            "Do you know what my favorite"
+        ],
+        middle: [
+            "when talent doesn’t",
+            "a person who",
+            "limits like fears are",
+            "things of yourself",
+            "talent than you but there’s no excuse",
+            "stopped trying to make them miss the",
+            "to prove there’s nothing",
+            "enough on their first wind",
+            "and over again in my life",
+            "part of the game is"
+        ],
+        end: [
+            "work hard",
+            "never gives up",
+            "often just an illusion",
+            "before you can do them",
+            "for anyone to work harder than you do",
+            "ball and started trying to make them hit it",
+            "greater than a challenge",
+            "to find out they’ve got a second",
+            "And that is why I succeed",
+            "The opportunity to play"
+        ]
+
+    }
+
+}
